09d09aa41969ea35cd6a5fc0d92c21983efb3ccc
Ray Waldin <rwaldin@signatureanalytics.com>
2021-09-18 13:45:18 -0700

    GH-14 fixes HR id

--- api/getEmbedToken/workspaces.js
+++ api/getEmbedToken/workspaces.js
@@ -23,6 +23,7 @@ const workspaces = {
         },
         users: {
             google: {
+<<<<<<< HEAD
                 'rwaldin@signatureanalytics.com': [
                     'HR',
                     'Revenue',
