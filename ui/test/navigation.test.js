a5429a5141be393d6338301ea1d2d17a2aecaebc
Ray Waldin <rwaldin@signatureanalytics.com>
2021-09-22 13:37:41 -0700

    GH-18 fixes nav test

--- ui/test/navigation.test.js
+++ ui/test/navigation.test.js
@@ -40,7 +40,7 @@ test.describe('Navigation', () => {
     test('should list all pages of current report', async ({ page }) => {
         await login(page, 'rwaldin@signatureanalytics.com');
         await page.goto('http://localhost:4280/signatureanalytics');
-        await page.waitForTimeout(5000);
+        await page.waitForTimeout(10000);
         await page.waitForSelector('.page');
         const pages = await page.evaluate(async _ => {
             const main = document.querySelector('sa-main');
