e8f393ce2f733bc1d537214ec6d8b561a7cf0d12
Ray Waldin <rwaldin@signatureanalytics.com>
2021-09-22 13:57:13 -0700

    GH-18 flailing

--- ui/test/navigation.test.js
+++ ui/test/navigation.test.js
@@ -39,9 +39,16 @@ test.describe('Navigation', () => {
 
     test('should list all pages of current report', async ({ page }) => {
         await login(page, 'rwaldin@signatureanalytics.com');
-        await page.waitForTimeout(5000);
+        const reports = [];
+        const responseListener = async response => {
+            if (response.url() === `http://localhost:4280/api/getEmbedToken`) {
+                const json = await response.json();
+                json.reports.forEach(r => reports.push(r));
+                page.off('response', responseListener);
+            }
+        };
         await page.goto('http://localhost:4280/signatureanalytics');
-        await page.waitForTimeout(5000);
+        page.on('response', responseListener);
         await page.waitForSelector('.page');
         const pages = await page.evaluate(async _ => {
             const main = document.querySelector('sa-main');
