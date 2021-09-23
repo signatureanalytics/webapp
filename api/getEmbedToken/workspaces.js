291732bd830a5a35df59c0a0638134fcb30493bc
Ray Waldin <rwaldin@signatureanalytics.com>
2021-09-23 14:44:16 -0700

    GH-12 update workspace

--- api/getEmbedToken/workspaces.js
+++ api/getEmbedToken/workspaces.js
@@ -1,32 +1,118 @@
 const workspaces = {
     signatureanalytics: {
+        id: 'f9b1241e-ef36-489b-bc2e-eba05b970c73',
+        reports: {
+            Engagement: {
+                id: '81309e92-a565-4923-b21a-db5a34e89ddf',
+            },
+            'Hours Detail': {
+                id: '8923f976-8955-4bbe-a699-bcc32200ff93',
+            },
+            HR: {
+                id: 'c33f6afb-6a37-41c5-873b-fa339a9edeb7',
+            },
+            Market: {
+                id: '780e87ed-3bed-4de7-830b-da5e1464dbe0',
+            },
+            Revenue: {
+                id: '993e591f-cbe7-47a4-b285-087ae4b6e5ee',
+            },
+            Sales: {
+                id: '11c867d4-6dd5-452f-b2c1-943a1f2eb3bc',
+            },
+        },
+        users: {
+            google: {
+                'rwaldin@signatureanalytics.com': [
+                    'HR',
+                    'Revenue',
+                    'Engagement',
+                    'Hours Detail',
+                    'Market',
+                ],
+                'cwagner@signatureanalyics.com': [
+                    'HR',
+                    'Revenue',
+                    'Engagement',
+                    'Hours Detail',
+                    'Market',
+                    'Sales',
+                ],
+                'dshannon@signatureanalytics.com': [
+                    'HR',
+                    'Revenue',
+                    'Engagement',
+                    'Hours Detail',
+                    'Market',
+                    'Sales',
+                ],
+                'arichards@signatureanalytics.com': [
+                    'HR',
+                    'Revenue',
+                    'Engagement',
+                    'Hours Detail',
+                    'Market',
+                    'Sales',
+                ],
+            },
+        },
+    },
+
+    sigan: {
         id: '77392655-0b1a-4155-a702-be372e795aa8',
         reports: {
-            engagement: {
+            Engagement: {
                 id: 'da0df838-dbe5-47e0-9c7c-526898624cc5',
             },
-            hoursdetail: {
+            'Hours Detail': {
                 id: '86877f86-17f3-4bc3-9cf3-7bcfceeec696',
             },
-            hr: {
+            HR: {
                 id: '1d29c950-edc1-400e-a56a-cd5b6189ed02',
             },
-            market: {
+            Market: {
                 id: 'bdcb9bb4-1595-46dc-baed-7d8162be82c1',
             },
-            revenue: {
+            Revenue: {
                 id: '9c544dc9-dc07-43dd-8191-f1ffe9eb45a7',
             },
-            sales: {
+            Sales: {
                 id: '8c45c507-8ed6-4165-9384-a7846e18f0d1',
             },
         },
         users: {
             google: {
-                'rwaldin@signatureanalytics.com': ['hr', 'revenue', 'engagement', 'hoursdetail', 'market'],
-                'cwagner@signatureanalyics.com': ['hr', 'revenue', 'engagement', 'hoursdetail', 'market', 'sales'],
-                'dshannon@signatureanalytics.com': ['hr', 'revenue', 'engagement', 'hoursdetail', 'market', 'sales'],
-                'arichards@signatureanalytics.com': ['hr', 'revenue', 'engagement', 'hoursdetail', 'market', 'sales'],
+                'rwaldin@signatureanalytics.com': [
+                    'HR',
+                    'Revenue',
+                    'Engagement',
+                    'Hours Detail',
+                    'Market',
+                ],
+                'cwagner@signatureanalyics.com': [
+                    'HR',
+                    'Revenue',
+                    'Engagement',
+                    'Hours Detail',
+                    'Market',
+                    'Sales',
+                ],
+                'dshannon@signatureanalytics.com': [
+                    'HR',
+                    'Revenue',
+                    'Engagement',
+                    'Hours Detail',
+                    'Market',
+                    'Sales',
+                ],
+                'arichards@signatureanalytics.com': [
+                    'HR',
+                    'Revenue',
+                    'Engagement',
+                    'Hours Detail',
+                    'Market',
+                    'Sales',
+                ],
             },
         },
     },
