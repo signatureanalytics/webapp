7233795459ae933f3a633f2403a2f5aff09cba63
Ray Waldin <rwaldin@signatureanalytics.com>
2021-10-07 13:03:59 -0700

    fixes empty operand

--- ui/scripts/test.sh
+++ ui/scripts/test.sh
@@ -2,5 +2,5 @@
 if [[ ! -d 'build' ]]; then
     npm run build
 fi 
-export npx=$([ $OS == "Windows_NT" ] && echo "npx.cmd" || echo "npx")
+export npx=$([ "$OS" == "Windows_NT" ] && echo "npx.cmd" || echo "npx")
 scripts/spawnWaitSpawn.js "$npx swa start build --api ../api" "emulator started" "$npx playwright test $*"
