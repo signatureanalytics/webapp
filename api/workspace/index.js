89e3d6f64ff41ee27048ecbdef8e9fae0e36648e
Ray Waldin <ray@waldin.net>
2022-01-25 13:13:39 -0800

    GH-61 adds custom metrics dimensions

--- api/workspace/index.js
+++ api/workspace/index.js
@@ -158,7 +158,6 @@ module.exports = async (context, req) => {
             token: getTokenJson.token,
             tokenExpires: getTokenJson.expiration,
             reports,
-            env: process.env,
         },
     };
 };
