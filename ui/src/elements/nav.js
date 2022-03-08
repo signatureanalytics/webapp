387c95891b7affdf23245fae607888977a12a6d3
Ray Waldin <ray@waldin.net>
2022-03-08 03:36:56 -0800

    scales star down

--- ui/src/elements/nav.js
+++ ui/src/elements/nav.js
@@ -97,7 +97,7 @@ class Nav extends ConnectedLitElement {
 
     #renderPageStar(workspaceSlug, report, page) {
         if (this.favoritePages.includes(`${workspaceSlug}/${slug(report.name)}/${slug(page.name)}`)) {
-            return html` <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="16px" viewBox="0 0 24 24" width="16px" fill="#000000"><g><path d="M0,0h24v24H0V0z" fill="none"/><path d="M0,0h24v24H0V0z" fill="none"/></g><g><path d="M12,17.27L18.18,21l-1.64-7.03L22,9.24l-7.19-0.61L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27z"/></g></svg>`
+            return html` <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="14px" viewBox="0 0 24 24" width="14px" fill="#000000"><g><path d="M0,0h24v24H0V0z" fill="none"/><path d="M0,0h24v24H0V0z" fill="none"/></g><g><path d="M12,17.27L18.18,21l-1.64-7.03L22,9.24l-7.19-0.61L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27z"/></g></svg>`
         }
         return '';
     }
