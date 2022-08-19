72e4ff4ea27b36cbbe29e086a8924ac3bd3b3d8e
Ray Waldin <ray@waldin.net>
2022-08-19 11:55:53 -0700

    adds sa logo

--- ui/src/logos.js
+++ ui/src/logos.js
@@ -2,10 +2,12 @@ import burdgeArchitects from './assets/burdgearchitects.png';
 import covantage from './assets/sa-covantage-logo.png';
 import harklinikken from './assets/harklinikken.png';
 import intelitics from './assets/intelitics.png';
+import signatureanalytics from './assets/sa-logo.png';
 
 export default {
     covantage,
     burdgeArchitects,
     harklinikken,
     intelitics,
+    signatureanalytics,
 };
